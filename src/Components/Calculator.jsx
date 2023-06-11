3222b86c0caa31b8a5b53ed2d516a9b47bfe501a
Oleg Shevchenko <72355488+Am4rel@users.noreply.github.com>
2023-06-11 13:24:51 +0000

    code updates

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -26,30 +26,30 @@ const Calculator = () => {
 
     const millisecondsBetween = dateTo.valueOf() - dateFrom.valueOf();
 return (
-<div className="text-center">
-    <h2 className="lead mt-5">Here you can choose two dates and get difference in time between them</h2>
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateTimePicker value={dateFrom} onAccept={onDateChange(true)} closeOnSelect label="Select start date" className="m-5"/>
-        <DateTimePicker value={dateTo} onAccept={onDateChange(false)} closeOnSelect label="Select end date" className="m-5"/>
-    </LocalizationProvider>
-    <div className="alert alert-danger visually-hidden" role="alert" ref={alertRef}>
-        It will be better if start date is less that end date, though...
-    </div>
-
-    {
-        showDifference && 
-        <div className='w-50 mx-auto d-flex justify-content-between'>
-            <div className="fw-medium">Difference in time between two dates is:</div>
-            <div className="text-end">
-                <p className="fw-medium">{millisecondsBetween.toLocaleString()} milliseconds.</p>
-                <p className="fw-medium">{Math.floor(millisecondsBetween / 1000).toLocaleString()} seconds.</p>
-                <p className="fw-medium">{Math.floor(millisecondsBetween / 1000 / 60).toLocaleString()} minutes.</p>
-                <p className="fw-medium">{Math.floor(millisecondsBetween / 1000 / 60 / 60).toLocaleString()} hours.</p>
-                <p className="fw-medium">{Math.floor(millisecondsBetween / 1000 / 60 / 60 / 24).toLocaleString()} days.</p>
-            </div>
+    <div className="text-center">
+        <h2 className="lead mt-5">Here you can choose two dates and get difference in time between them</h2>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateTimePicker value={dateFrom} onAccept={onDateChange(true)} closeOnSelect label="Select start date" className="m-5"/>
+            <DateTimePicker value={dateTo} onAccept={onDateChange(false)} closeOnSelect label="Select end date" className="m-5"/>
+        </LocalizationProvider>
+        <div className="alert alert-danger visually-hidden" role="alert" ref={alertRef}>
+            It will be better if start date is less that end date, though...
         </div>
-    }
-</div>
+
+        {
+            showDifference && 
+            <div className='mx-auto d-md-flex flex-wrap justify-content-md-between' style={{ maxWidth: 600 }}>
+                <div className="fw-medium mx-auto mb-3" style={{ width: 320 }}>Difference in time between two dates is:</div>
+                <div className="text-md-end mx-auto"  style={{ width: 200 }}>
+                    <p>{millisecondsBetween.toLocaleString()} milliseconds.</p>
+                    <p>{Math.floor(millisecondsBetween / 1000).toLocaleString()} seconds.</p>
+                    <p>{Math.floor(millisecondsBetween / 1000 / 60).toLocaleString()} minutes.</p>
+                    <p>{Math.floor(millisecondsBetween / 1000 / 60 / 60).toLocaleString()} hours.</p>
+                    <p>{Math.floor(millisecondsBetween / 1000 / 60 / 60 / 24).toLocaleString()} days.</p>
+                </div>
+            </div>
+        }
+    </div>
 )
 }
 export default Calculator;
